5ec84ce8f5debe625160d21c4b58abedfbba3a63
profalves <igo.sud@hotmail.com>
2018-01-16 12:57:14 -0300

    mudando a tela de clientes

--- src/themes/notifications.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import localforage from 'localforage'
-    
-const options = localforage.config({
-    driver: localforage.INDEXEDDB,
-    name: 'I-heart-localStorage'
-});
-
-Vue.use(localforage, options)
